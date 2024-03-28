e3f04821bc6c057ce2611a3aca5d80a36dcc891a
Alexey Malashkevich <m.alexey@gmail.com>
2024-03-28 12:11:12 -0400

    Use OpenZeppelin contracts

--- src/interfaces/ISwapRouter02.sol
+++ src/interfaces/ISwapRouter02.sol
@@ -2,33 +2,33 @@
 pragma solidity ^0.8.0;
 
 interface ISwapRouter02 {
-    struct ExactInputSingleParams {
-        address tokenIn;
-        address tokenOut;
-        uint24 fee;
-        address recipient;
-        uint256 amountIn;
-        uint256 amountOutMinimum;
-        uint160 sqrtPriceLimitX96;
-    }
+  struct ExactInputSingleParams {
+    address tokenIn;
+    address tokenOut;
+    uint24 fee;
+    address recipient;
+    uint256 amountIn;
+    uint256 amountOutMinimum;
+    uint160 sqrtPriceLimitX96;
+  }
 
-    function exactInputSingle(ExactInputSingleParams calldata params)
-        external
-        payable
-        returns (uint256 amountOut);
+  function exactInputSingle(ExactInputSingleParams calldata params)
+  external
+  payable
+  returns (uint256 amountOut);
 
-    struct ExactOutputSingleParams {
-        address tokenIn;
-        address tokenOut;
-        uint24 fee;
-        address recipient;
-        uint256 amountOut;
-        uint256 amountInMaximum;
-        uint160 sqrtPriceLimitX96;
-    }
+  struct ExactOutputSingleParams {
+    address tokenIn;
+    address tokenOut;
+    uint24 fee;
+    address recipient;
+    uint256 amountOut;
+    uint256 amountInMaximum;
+    uint160 sqrtPriceLimitX96;
+  }
 
-    function exactOutputSingle(ExactOutputSingleParams calldata params)
-        external
-        payable
-        returns (uint256 amountIn);
+  function exactOutputSingle(ExactOutputSingleParams calldata params)
+  external
+  payable
+  returns (uint256 amountIn);
 }
--- test/TokenSwap.t.sol
+++ test/TokenSwap.t.sol
@@ -1,44 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../src/interfaces/ISwapRouter02.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {TokenSwap} from "../src/TokenSwap.sol";
 
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount)
-        external
-        returns (bool);
-    function allowance(address owner, address spender)
-        external
-        view
-        returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount)
-        external
-        returns (bool);
-}
 
 contract TokenSwapTest is Test {
-    TokenSwap public tokenSwap;
+  TokenSwap public tokenSwap;
 
-    address constant SWAP_ROUTER_02 = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
-    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    ISwapRouter02 private constant router = ISwapRouter02(SWAP_ROUTER_02);
+  address constant SWAP_ROUTER_02 = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
+  address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+  ISwapRouter02 private constant router = ISwapRouter02(SWAP_ROUTER_02);
 
-    function setUp() public {
-        tokenSwap = new TokenSwap();
-    }
+  function setUp() public {
+    tokenSwap = new TokenSwap();
+  }
 
-    function test_Swap() public {
-        uint256 amountIn = 1 * 1e18;
-        IERC20 weth = IERC20(WETH);
+  function test_Swap() public {
+    uint256 amountIn = 1 * 1e18;
+    IERC20 weth = IERC20(WETH);
 
-        vm.startPrank(0xF04a5cC80B1E94C69B48f5ee68a08CD2F09A7c3E);
-        weth.approve(address(tokenSwap), amountIn);
+    vm.startPrank(0xF04a5cC80B1E94C69B48f5ee68a08CD2F09A7c3E);
+    weth.approve(address(tokenSwap), amountIn);
 
-        tokenSwap.swapExactInputSingleHop(amountIn, 2000 * 1e18);
-    }
+    tokenSwap.swapExactInputSingleHop(amountIn, 2000 * 1e18);
+  }
 }
